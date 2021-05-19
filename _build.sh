8ac064277a299177ffe0911a46279ccc08482439
shainarace <slrace@ncsu.edu>
2021-05-19 18:46:09 -0400

    SR_Third Commit

--- _build.sh
+++ _build.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
-
 set -ev
 
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook', clean=F)"
+bookdown::render_book("index.Rmd"bookdown::render_book("index.Rmd"))
+
+Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook', clean=T)"
+
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
 
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
+
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
+
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::html_book')"
 
 
